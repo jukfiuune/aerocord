aedf882eae5c8e3047292664c7299dd88ddbc3bc
patricktbp <135556230+n1d3v@users.noreply.github.com>
2024-07-24 19:38:19 +0100

    make sysinfo.cs better, was having some issues with reg values in 11

--- Aerocord/SysInfo.cs
+++ Aerocord/SysInfo.cs
@@ -7,40 +7,58 @@ namespace Aerocord
     {
         public static string GetRegistryValue(string Path, string ValueName, string Default)
         {
-                string value = Default;
-                RegistryHive Hive;
-                switch (Path.Split('\\')[0])
+            string value = Default;
+            RegistryHive Hive;
+
+            // Determine the registry hive based on the path
+            switch (Path.Split('\\')[0])
+            {
+                case "HKEY_CLASSES_ROOT":
+                    Hive = RegistryHive.ClassesRoot;
+                    break;
+                case "HKEY_CURRENT_USER":
+                    Hive = RegistryHive.CurrentUser;
+                    break;
+                case "HKEY_LOCAL_MACHINE":
+                    Hive = RegistryHive.LocalMachine;
+                    break;
+                case "HKEY_USERS":
+                    Hive = RegistryHive.Users;
+                    break;
+                case "HKEY_CURRENT_CONFIG":
+                    Hive = RegistryHive.CurrentConfig;
+                    break;
+                default:
+                    Hive = RegistryHive.LocalMachine;
+                    break;
+            }
+
+            try
+            {
+                using (RegistryKey baseKey = RegistryKey.OpenBaseKey(Hive, Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32))
                 {
-                    case "HKEY_CLASSES_ROOT":
-                        Hive = RegistryHive.ClassesRoot;
-                        break;
-                    case "HKEY_CURRENT_USER":
-                        Hive = RegistryHive.CurrentUser;
-                        break;
-                    case "HKEY_LOCAL_MACHINE":
-                        Hive = RegistryHive.LocalMachine;
-                        break;
-                    case "HKEY_USERS":
-                        Hive = RegistryHive.Users;
-                        break;
-                    case "HKEY_CURRENT_CONFIG":
-                        Hive = RegistryHive.CurrentConfig;
-                        break;
-                    default:
-                        Hive = RegistryHive.LocalMachine;
-                        break;
+                    using (RegistryKey localKey = baseKey.OpenSubKey(Path.Substring(Path.IndexOf('\\') + 1)))
+                    {
+                        if (localKey != null)
+                        {
+                            object regValue = localKey.GetValue(ValueName);
+                            if (regValue != null)
+                            {
+                                value = regValue.ToString();
+                            }
+                        }
+                    }
                 }
-                RegistryKey localKey =
-                    RegistryKey.OpenBaseKey(Hive,
-                        Environment.Is64BitOperatingSystem ? RegistryView.Registry64 : RegistryView.Registry32);
-                localKey = localKey.OpenSubKey(Path.Substring(Path.IndexOf('\\') + 1));
-                if (localKey != null)
-                {
-                    value = localKey.GetValue(ValueName).ToString();
-                    localKey.Close();
-                }
-                return value;
+            }
+            catch (Exception ex)
+            {
+                // Optionally log the exception
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+
+            return value;
         }
+
         public static string GetVersionString()
         {
             return GetRegistryValue(@"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion", "LCUVer", "6.1.7601.17514");
